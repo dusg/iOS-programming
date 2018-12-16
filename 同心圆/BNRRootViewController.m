70cc913614d353f0a9580ac952eafa1b8870fdd6
dusg <siguang_du@c-sky.com>
2018-12-16 10:47:49 +0800

    拖动分页

--- 同心圆/BNRRootViewController.m
+++ 同心圆/BNRRootViewController.m
@@ -14,14 +14,23 @@
     UIScrollView *scrollView = [[UIScrollView alloc] init];
     self.view = scrollView;
 
-    CGRect bigRect = [[UIScreen mainScreen] bounds];
-    bigRect.size.height *= 2;
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGRect bigRect = screenRect;
+//    bigRect.size.height *= 2;
     bigRect.size.width *= 2;
 
-    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:bigRect];
+//    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:bigRect];
+    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
 
     [scrollView addSubview:hypnosisView];
+
+    screenRect.origin.x += screenRect.size.width;
+    BNRHypnosisView *anotherView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherView];
+
     scrollView.contentSize = bigRect.size;
+
+    [scrollView setPagingEnabled:YES];
 }
 
 @end
