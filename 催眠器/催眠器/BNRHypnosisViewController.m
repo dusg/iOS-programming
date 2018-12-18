cbe0c95374e558e6e72c6e1933bab03440fba411
dusg <siguang_du@c-sky.com>
2018-12-18 22:36:16 +0800

    为每个标签添加视差效果

--- 催眠器/催眠器/BNRHypnosisViewController.m
+++ 催眠器/催眠器/BNRHypnosisViewController.m
@@ -9,11 +9,54 @@
 #import "BNRHypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
-@interface BNRHypnosisViewController ()
+@interface BNRHypnosisViewController () <UITextFieldDelegate>
 
 @end
 
 @implementation BNRHypnosisViewController
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"textField.text = %@", textField.text);
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)drawHypnoticMessage:(NSString *)text {
+    for (int i = 0; i < 20; ++i) {
+        UILabel *label = [[UILabel alloc] init];
+        // 设置UILabel对象的文字和颜色
+        label.backgroundColor = [UIColor clearColor];
+        label.textColor = [UIColor whiteColor];
+        label.text = text;
+        // 根据需要显示的文字调整UILabel对象的大小
+        [label sizeToFit];
+        // 获取随机x坐标，
+        // 使UILabel对象的宽度不超出BNRHypnosisViewController的view宽度
+        int width = self.view.bounds.size.width - label.bounds.size.width;
+        int x = arc4random() % width;
+        // 使UILabel对象的宽度不超出BNRHypnosisViewController的view宽度
+        int height = self.view.bounds.size.height - label.bounds.size.height;
+        int y = arc4random() % height;
+
+        CGRect rect = label.frame;
+        rect.origin = CGPointMake(x, y);
+        label.frame = rect;
+
+        [self.view addSubview:label];
+        //添加视差效果
+        UIInterpolatingMotionEffect *motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @(-25);
+        motionEffect.maximumRelativeValue = @(25);
+        [label addMotionEffect:motionEffect];
+
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        motionEffect.minimumRelativeValue = @(-25);
+        motionEffect.maximumRelativeValue = @(25);
+        [label addMotionEffect:motionEffect];
+    }
+}
 
 - (void)loadView {
     [super loadView];
@@ -23,6 +66,9 @@
     CGRect rect = CGRectMake(40, 100, 300, 30);
     UITextField *textField = [[UITextField alloc] initWithFrame:rect];
     textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"催眠我";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
     [backgroundView addSubview:textField];
 
     self.view = backgroundView;
