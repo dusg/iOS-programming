25621dac37f0c6ae218e9777e6330f5acb4d9374
dusg <siguang_du@c-sky.com>
2018-12-22 16:52:37 +0800

    增加编辑模式

--- 房子着火/房子着火/BNRItemsViewController.m
+++ 房子着火/房子着火/BNRItemsViewController.m
@@ -6,20 +6,61 @@
 //  Copyright © 2018 杜. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import "BNRItemsViewController.h"
 #import "BNRItemStore.h"
 
 @interface BNRItemsViewController()<UITableViewDataSource>
-{
 
-}
+@property (nonatomic, strong) IBOutlet UIView *headerView;
+
 @end
 
 @implementation BNRItemsViewController
+- (instancetype)init {
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if (self) {
+        for (int i = 0; i < 5; ++i) {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
+
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    return [self init];
+}
+
+- (IBAction)addNewItem:(id)sender {
+}
+
+- (IBAction)toggleEditingMode:(id)sender {
+    if (self.isEditing) {
+        [sender setTitle:@"编辑" forState:UIControlStateNormal];
+        [self setEditing:NO  animated:YES];
+    } else {
+        [sender setTitle:@"完成" forState:UIControlStateNormal];
+        [self setEditing:YES  animated:YES];
+    }
+}
+
+//getter and setter
+- (UIView *)headerView {
+    if (!_headerView) {
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderVie" owner:self options:nil];
+    }
+    return _headerView;
+}
+
+
+//delegate method
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class]
             forCellReuseIdentifier:@"UITableViewCell"];
+
+    [self.tableView setTableHeaderView:self.headerView];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -38,19 +79,5 @@
     return [[[BNRItemStore sharedStore] allItems] count];
 }
 
-- (instancetype)init {
-    self = [super initWithStyle:UITableViewStylePlain];
-    if (self) {
-        for (int i = 0; i < 5; ++i) {
-            [[BNRItemStore sharedStore] createItem];
-        }
-    }
-
-    return self;
-}
-
-- (instancetype)initWithStyle:(UITableViewStyle)style {
-    return [self init];
-}
 
 @end
