164499c28e6372ada9e42678b7cb35a5c5763c7a
dusg <siguang_du@c-sky.com>
2018-12-23 13:53:17 +0800

    添加BNRDetailViewController

---
+++ 房子着火/房子着火/UI Controller/BNRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.h
+//  房子着火
+//
+//  Created by Du Carl on 2018/12/23.
+//  Copyright © 2018 杜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 房子着火/房子着火/UI Controller/BNRDetailViewController.m
@@ -0,0 +1,23 @@
+//
+//  BNRDetailViewController.m
+//  房子着火
+//
+//  Created by Du Carl on 2018/12/23.
+//  Copyright © 2018 杜. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation BNRDetailViewController
+
+
+@end
